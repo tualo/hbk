eb441eaa61978577c87eb75f820c177ca9de1b3a
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2022-09-22 10:15:50 +0200

    add middleware

--- src/functions.php
+++ src/functions.php
@@ -1,6 +1,9 @@
 <?php
 
 require_once "HlsHelper.php";
+
+require_once "Middlewares/Middlewares.php";
+
 require_once "Routes/CleanJobs.php";
 require_once "Routes/GetFiles.php";
 require_once "Routes/GlobFiles.php";
